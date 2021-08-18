62db46d48e7a9532c34cd70bded293497f9b9518
hallefcalves <48840344+hallefcalves@users.noreply.github.com>
2021-08-18 09:08:00 -0300

    Primeiro commit

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.model('alurapic').controller('FotosController', function($scope){
+
+    $scope.foto = {
+        titulo : 'Leão',
+        url: 'https://www.infoescola.com/wp-content/uploads/2017/04/leao-126767138.jpg'
+    }
+});
