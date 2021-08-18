6cd49c55151985404d5d0bff56f924bc2568f4dd
hallefcalves <48840344+hallefcalves@users.noreply.github.com>
2021-08-18 16:23:19 -0300

    Aula 1

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,7 +1,7 @@
-angular.model('alurapic').controller('FotosController', function($scope){
+angular.module('alurapic').controller('FotosController', function($scope){
 
     $scope.foto = {
         titulo : 'Leão',
         url: 'https://www.infoescola.com/wp-content/uploads/2017/04/leao-126767138.jpg'
-    }
+    };
 });
